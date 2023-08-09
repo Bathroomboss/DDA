8069eebab49932fae29735b6c7d40255d7a7c4b6
Bathroomboss <113091536+Bathroomboss@users.noreply.github.com>
2023-08-09 12:40:21 +0800

    Update HTGTS_mapping.sh

--- source code/HTGTS/HTGTS_mapping.sh
+++ source code/HTGTS/HTGTS_mapping.sh
@@ -14,7 +14,7 @@ cutadapt  -e 0.1 -O 3 -m 55 --quality-cutoff 25 -a AGATCGGAAGAG -A AGATCGGAAGAG 
 -o cutadapt/${Filename}_cutadapt.R1_fastq.gz -p cutadapt/${Filename}_cutadapt_R2.fastq.gz  ${Filename}_1.fq.gz ${Filename}_2.fq.gz
 TranslocWrapper.pl metadata.txt cutadapt/ results/ --threads 8  --pipeline-opt --no-dedup 
 
-awk 'BEGIN{FS=OFS="\t"}{if ($5==1) print $3,$6,$7,$1,$18,"+";else print $3,$6,$7,$1,$18,"-"}'  $File.txt > $File.bed
+awk 'BEGIN{FS=OFS="\t"}{if ($5==1) print $3,$6,$7,$1,$18,"+";else print $3,$6,$7,$1,$18,"-"}'  ${File}_result.tlx > $File.bed
 
 sed -i '1d' $File.bed
 sort -k1,1 -k2,2n $File.bed |bedtools intersect -a - -b $Blacklist -v > $File.sorted.bed
